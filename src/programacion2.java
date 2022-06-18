fbd8074350722ffd9dd7e2e64078a8b1f31059b9
jaimito1995 <jaimecaicedo.jc@hotmail.com>
2022-06-18 09:17:56 -0500

    fin del ejercicio

--- src/programacion2.java
+++ src/programacion2.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class programacion2 {
+import javax.swing.*;
+
+public class programacion2 {
+    public static void main(String args[]){
+        int pesos = Integer.parseInt(JOptionPane.showInputDialog("ingresar valor en pesos"));
+        double bolivar = (0.0014 * pesos);
+        JOptionPane.showMessageDialog(null,"el valor de los pesos en bolivares es" + bolivar);
+
+    }
 }
